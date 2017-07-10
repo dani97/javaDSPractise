7075aea2cc81290bcc53dbb9251814f5d0841cbc
christopher daniel <christopherdaniel13397@gmail.com>
2017-07-10 14:15:28 +0530

    Update Node.java

--- bsts/Node.java
+++ bsts/Node.java
@@ -1,20 +1,12 @@
 package bsts;
 
 import java.util.Scanner;
-
-public class RotateTree {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		BST b = new BST();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i=0;i<n;i++){
-			b.insert(sc.nextInt());
-		}
-		sc.close();
-		
-		b.display();
-		mirrorTree(b.root);
-		b.display();
+public class Node{
+	public int data;
+	public Node left;
+	public Node right;
+	Node(){
+		this.left=null;
+		this.right=null;
 	}
+}
